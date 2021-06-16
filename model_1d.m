2011ff04a48f6e89eb52d1b0475c8032e3a9e10a
Chongan <wca0404@gmail.com>
2021-06-16 11:10:57 +0200

    1d model run with plot

--- model_1d.m
+++ model_1d.m
@@ -15,7 +15,7 @@ beta = 1;
 Fz = 1;
 d = 1;
 k = 1/2;
-h = 1/128;  %minimun gird spacing
+h = 1/10000;  %minimun gird spacing
 num = fix(2 * RInitShrink / h);
 r = linspace(0, 2 * RInitShrink, num);
 
@@ -59,18 +59,45 @@ phi(1,:) = 0.5 * (1 - tanh((r-RInitGrow)/(sqrt(8) * epi)));
 
 plot(r,phi(1,:));
 hold on;
+title('phi changing with time');
+xlabel('r');
+ylabel('phi');
+
+R = zeros(1,20000);
+R(1) = RInitGrow;
 
 for j = 2:20000
+    
+    % updating velocity
+    SVcGrow = (1 - 2 * v) * Fz / (E * pi * R(j-1)^2);
+    cVcGrow = SVcGrow / k;
+    VlinVcGrow = alphaVcGrow * cVcGrow - beta;
+    
     for i = 2:num-1
-        phi(j,i) = phi(j-1,i) + dt * (-VlinVcGrow * (phi(j-1,i) - phi(j-1,i-1))/h + ...(-phi(j-1,i) * (1 - phi(j-1,i)) / epi) + ...
+        phi(j,i) = phi(j-1,i) + dt * (-VlinVcGrow * (-phi(j-1,i) * (1 - phi(j-1,i)) / epi) + ...
             gamma * (-phi(j-1,i)^3 + 1.5 * phi(j-1,i)^2 - 0.5 * phi(j-1,i)) + ...
-            gamma * epi^2 * (phi(j-1,i-1) - 2 * phi(j-1,i) + phi(j-1,i+1))/h^2);...(phi(j-1,i) - phi(j-1,i-1))^2 / h^2);    
+            gamma * epi^2 * (phi(j-1,i) - phi(j-1,i-1))^2 / h^2);  
+        
+        if abs(phi(j,i) - 0.5) < abs(phi(j,i-1) - 0.5)
+            R(j) = r(i);
+        end
     end
+    
     if mod(j,200) == 0
         plot(r,phi(j,:));
     end
     
 end
+
+figure(2);
+t = linspace(1,20000,20000)* dt;
+plot(t, pi * R.^2);
+title('Volume varying with time');
+xlabel('time/s');
+ylabel('Volume');
+ylim([0.15,0.65]);
+grid on;
+
 
 %% Working with funtions in SOLIDIFICATION
 
@@ -79,8 +106,12 @@ end
 % phi(:,num) = 0;
 % 
 % phi(1,:) = (1 -tanh((r - RInitGrow)/2/epi))/2;
+% 
 % plot(r,phi(1,:));
 % hold on;
+% title('phi changing with time');
+% xlabel('r');
+% ylabel('phi');
 % 
 % 
 % for j = 1 : 20000
@@ -89,29 +120,30 @@ end
 %             gamma * epi^2 * ((phi(j,i-1) - 2 * phi(j,i) + phi(j,i+1)) / h^2 + ...
 %             phi(j,i) * (1 - phi(j,i)) * (1 - 2 * phi(j,i)) / epi^2 ));
 %     end
-%     if mod(j,200) == 0
+%     if mod(j,100) == 0
 %         plot(r,phi(j,:));
 %     end
 % end
-% plot(r,phi(200,:));
-% 
+
 
 %% Testing with funtions in SHARP INTERFACE
 % Working but wrong direction
 
-% phi = zeros(200,64);
+% phi = zeros(20000,num);
 % phi(:,1) = 1;
-% phi(:,64) = -1;
+% phi(:,num) = -1;
 % 
-% phi(1,:) = -tanh(r/sqrt(2)/epi);
+% phi(1,:) = -tanh((r-RInitGrow)/sqrt(2)/epi);
 % plot(r,phi(1,:));
 % hold on;
 % 
-% for j = 1 : 199
-%     for i = 2 : 63
+% for j = 1 : 20000
+%     for i = 2 : num-1
 %         phi(j+1,i) = phi(j,i) + dt * (-VlinVcGrow * (1 - phi(j,i)^2) / sqrt(2) / epi + ...
 %             gamma * epi^2 * ((phi(j,i) - phi(j,i-1))^2 / h^2 + ...
 %             phi(j,i) * (1 - phi(j,i)^2) / epi^2 ));
 %     end
+%     if mod(j,200) == 0
+%         plot(r,phi(j,:));
+%     end
 % end
-% plot(r,phi(200,:));
